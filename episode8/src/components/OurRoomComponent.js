887bde903c3f0cc7e46ce2021f01a8cc1f1f7852
khanzzirfan <khanzz_irfan@hotmail.com>
2018-02-18 11:15:48 +1300

    added two new components

---
+++ episode8/src/components/OurRoomComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class OurRoomComponent extends Component {
+    render() {
+        return (
+            <div id="about-hotel" className="mt-3 col-xs-12 col-sm-12 col-md-12 col-centered center-block">
+                <div className="row">
+                    <div className="title text-center">
+                        <h6>Accomodation</h6>
+                        <h3>Our Rooms & Suites</h3>
+                        <div className="col-xs-6 col-sm-4 col-md-4 cap col-centered center-block">
+                            <span>
+                                <img alt="" src="http://themesquared.com/hotelier/wp-content/themes/hotelier/framework/builder/modules/heading/cap.png"
+                                    className="img-responsive" />
+                            </span>
+                        </div>
+                        <p>Duis sed odio sit amet nibh vulputate cursus a sit amet mauris. Morbi versa accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a ornare odio. Sed non mauris vitae erat consequat auctor eu in elit</p>        </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+OurRoomComponent.propTypes = {
+
+};
+
+export default OurRoomComponent;
--- episode8/src/containers/home-container.js
+++ episode8/src/containers/home-container.js
@@ -8,8 +8,11 @@ import logo from '../logo.png';
 import '../App.css';
 import RoomComponent from '../components/RoomComponent';
 import SlideShow from '../components/SlideShow';
+import AboutHotel from '../components/AboutHotel';
+import OurRoomComponent from '../components/OurRoomComponent';
 
 class HomeContainer extends Component {
+
     componentDidMount() {
         const { login, fetchRooms } = this.props;
         login();
@@ -25,16 +28,18 @@ class HomeContainer extends Component {
                 <div className="container-full">
                     <div className="container w-100">
                         <div className="row">
-                            <div className="col-md-6 ">
-                                <div className="row text-right">
-                                    <div className="col-md-12">
-                                        <img src={logo} width={250} alt="Redux Hotel" />
-                                        <h1>Your Reservation</h1>
-                                        <p>Name: {userName}</p>
-                                        <h2>Accomodation</h2>
-                                        <p><em>{accomodation.name}</em></p>
-                                        <p><img src={accomodation.image} width={300} alt="accomodation" /></p>
+                            <div className="col-md-6">
+                                <div id="redux-inn-logo" className="row">
+                                    <div className="col-sm-8 text-right">
+                                        <img className="" src={logo} width={250} alt="Redux Hotel" />
                                     </div>
+                                    <div className="col-sm-4 text-right">
+                                        <span className="text-right">Name: {userName}</span>    
+                                    </div>
+                                </div>
+                                
+                                <div className="row mt-5">
+                                    <OurRoomComponent />
                                 </div>
                                 <hr className="white" />
                                 <div className="row ">
